2001674167de313f6c9c0fb50fc8bc0ac2de6d56
JAMAL-111 <jamalkasule123@gmail.com>
2026-03-10 14:10:06 -0400

    Created Book Mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -112,6 +112,23 @@ const Mutation = new GraphQLObjectType({
                 });
                 return author.save();
             }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString },
+                genre: { type: GraphQLString },
+                authorId: { type: GraphQLID } 
+            },
+            //creating a new instance of the book
+            resolve(parent, args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
         }
     }
 })
